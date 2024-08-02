dea932d371b01a503e6f1c76993a37fa738b4bfe
Tuna <tuna@oceannet.com.tr>
2024-08-02 14:26:06 +0300

    init:start next js webpage limamia

---
+++ src/app/components/navbar/Navbar.tsx
@@ -0,0 +1,64 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import styles from "./navbar.module.css";
+
+const links = [
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "Portfolio",
+    url: "/portfolio",
+  },
+  {
+    id: 3,
+    title: "Blog",
+    url: "/blog",
+  },
+  {
+    id: 4,
+    title: "About",
+    url: "/about",
+  },
+  {
+    id: 5,
+    title: "Contact",
+    url: "/contact",
+  },
+  {
+    id: 6,
+    title: "Dashboard",
+    url: "/dashboard",
+  },
+];
+
+function Navbar() {
+  return (
+    <div className={styles.container}>
+      <Link className={styles.logo} href="/">
+        Lamamia
+      </Link>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <Link className={styles.link} href={link.url} key={link.id}>
+            {link.title}
+          </Link>
+        ))}
+        <button
+          onClick={() => {
+            console.log("logged out");
+          }}
+          className={styles.logout}
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./page.module.css";
+
+function Contact() {
+  return <div className={styles.contact}>Contact</div>;
+}
+
+export default Contact;
