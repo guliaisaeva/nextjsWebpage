b1196a5d1aa92ed966318ea8cedbeef6b70d66cc
Gulia <gulya.isaeva.pls@gmail.com>
2024-08-02 16:14:13 +0300

    feat:add about and coitact pages with style

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,30 @@
 import React from "react";
 import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "../components/Button/Button";
 
 function Contact() {
-  return <div className={styles.contact}>Contact</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Let's Keep In Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.image}
+            src="/contact.png"
+            alt="hello"
+            fill={true}
+          />
+        </div>
+        <form className={styles.form}>
+          <input type="text" placeholder="name" className={styles.input} />
+          <input type="text" placeholder="email" className={styles.input} />
+          <textarea className={styles.textArea} cols={30} rows={10}></textarea>
+          <Button text="Send" url="#" />
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Hero from "../../public/hero.png";
+import Button from "@/app/components/Button/Button";
 // import Button from "@/components/Button/Button";
 
 export default function Home() {
@@ -14,8 +15,7 @@ export default function Home() {
           Turning your Idea into Reality. We bring together the teams from the
           global tech industry.
         </p>
-        <button className={styles.button}>See Our Works</button>
-        {/* <Button url="/portfolio" text="See Our Works" /> */}
+        <Button url="/portfolio" text="See Our Works" />
       </div>
       <div className={styles.item}>
         <Image src={Hero} alt="hero" className={styles.img} />
